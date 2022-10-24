4dffd7f8424076ffa14eef683a06560fbd760f8d
Amoh Benjamin Koranteng <91298767+AmohBen1@users.noreply.github.com>
2022-10-24 17:45:23 -0400

    Update Visualization.py

--- Visualization.py
+++ Visualization.py
@@ -1,8 +1,25 @@
 # Import netowrkx to visualize our graphs. 
-import networkx as nx
+import dash 
+import visdcc
+import pandas as pd
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output, State
 
 from Games import Single_Stage_Game
 from Agent import Agent
 
+
+# Create app
+app = dash.Dash()
+
+# Load data
 data = Single_Stage_Game(0, N, agents, past_games= None, threshold = 0.5, time_limit= None)
-nx.draw_spring(data)
+
+# Definen layout 
+app.layout = html.Div([ 
+  visdcc.Network(id = 'net', 
+                 data = {'nodes': nodes, 'edges': edges},
+                 options = dict(height = '600px', width = '100%')),
+  dcc.RadioItems(id = 'color',
+                 options = [{'label': 'Red

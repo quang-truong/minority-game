e34914b2023ca5ecf34bd52a5cef413a7e0751e2
Amoh Benjamin Koranteng <91298767+AmohBen1@users.noreply.github.com>
2022-10-24 17:03:34 -0400

    Update Visualization.py

--- Visualization.py
+++ Visualization.py
@@ -1,3 +1,8 @@
 # Import netowrkx to visualize our graphs. 
 import networkx as nx
 
+from Games import Single_Stage_Game
+from Agent import Agent
+
+data = Single_Stage_Game(0, N, agents, past_games= None, threshold = 0.5, time_limit= None)
+nx.draw_spring(data)

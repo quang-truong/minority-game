66080f0f525a46fbcac7224aa86d9abbcf7a86d3
AmohBen1 <91298767+AmohBen1@users.noreply.github.com>
2022-11-06 01:14:38 -0400

    Ball tile content
    This contains the content for the tile that the ball will move in.

---
+++ Ball_tile.py
@@ -0,0 +1,9 @@
+# This file creates the main tile window that the ball rans i.
+import turtle as tr
+
+screen = tr.Screen()
+screen.setup(width=1000, height=600)
+screen.bgcolor('black')
+screen.title('Random_balls')
+
+tr.mainloop()
